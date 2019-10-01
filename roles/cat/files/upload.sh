eb474d97da2f267bc72dcdf2aacec26e07b35bd4
reireias <reireias@gmail.com>
2019-10-01 21:54:55 +0900

    add upload and motion daemon

--- roles/cat/files/upload.sh
+++ roles/cat/files/upload.sh
@@ -3,7 +3,8 @@
 TARGET_DIR="/tmp/motion"
 
 mkdir -p ${TARGET_DIR}
+rm "${TARGET_DIR}/*"
 while inotifywait -e CREATE ${TARGET_DIR}; do
     file=$(ls -rt ${TARGET_DIR} | tail -n 1)
-    gsutil -o 'Credentials:gs_service_key_file=/root/.credentials.json' cp "${TARGET_DIR}/${file}" gs://cat-watcher.appspot.com/
+    gsutil -o 'Credentials:gs_service_key_file=/root/.credentials.json' cp "${TARGET_DIR}/${file}" gs://cat-watcher.appspot.com/ && rm "${TARGET_DIR}/${file}"
 done
